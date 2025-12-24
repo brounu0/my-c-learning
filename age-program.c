33114104b58426b40f4bb2e35e58c5630b8730f2
brounu0 <walidwalidxd10@gmail.com>
2025-12-24 21:27:24 +0100

    Create age-program.c

---
+++ age-program.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("How old are you?");
+    scanf("%d", &age);
+    printf("Wow! you are %d years old.", age);
+
+    return 0;
+}
